f1982a4294f94816b966f31c00255f9810cc323c
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2021-12-22 17:27:13 -0300

    Fetching posts and comments from query service

--- client/src/components/CommentList.js
+++ client/src/components/CommentList.js
@@ -1,28 +1,11 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
-const CommentList = ({ postId }) => {
-	const [comments, setComments] = useState([]);
-
-	const fetchData = async () => {
-		const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
-		setComments(res.data);
-	};
-
-	const renderComments = comments.map(comment => {
-		return (
-			<li key={comment.id}>{comment.content}</li>
-		);
+const CommentList = ({ comments }) => {
+	const renderComments = comments.map((comment) => {
+		return <li key={comment.id}>{comment.content}</li>;
 	});
 
-	useEffect(() => {
-		fetchData();
-	}, []);
-	return (
-		<ul>
-			{renderComments}
-		</ul>
-	);
+	return <ul>{renderComments}</ul>;
 };
 
 export default CommentList;
